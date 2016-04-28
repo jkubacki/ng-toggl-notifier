22574886effb5022f07ec6037a686561d71bc52a
Jakub Kubacki <kubacki.jk@gmail.com>
2016-04-28 23:33:24 +0200

    Formatted entries
    With formatted date & entry duration

--- lib/monthly_user_report.rb
+++ lib/monthly_user_report.rb
@@ -10,4 +10,31 @@ class MonthlyUserReport
     @invalid_dinner_entries = []
     @invalid_project_entries = []
   end
+
+  def formatted_entries(entry_type)
+    entries = entry_type == :dinner ? invalid_dinner_entries : invalid_project_entries
+    entries.map do |entry|
+      {
+        start: format_date(entry['start']),
+        end: format_date(entry['end']),
+        duration: format_duration(entry['dur'])
+      }
+    end
+  end
+
+  private
+
+  def format_date(date)
+    Time.iso8601(date).strftime("%Y-%m-%d %H:%M:%S")
+  end
+
+  def format_duration(miliseconds)
+    x = miliseconds / 1000
+    seconds = x % 60
+    x /= 60
+    minutes = x % 60
+    x /= 60
+    hours = x % 24
+    [hours, minutes, seconds].map { |e| e.to_s.rjust(2,'0') }.join ':'
+  end
 end
