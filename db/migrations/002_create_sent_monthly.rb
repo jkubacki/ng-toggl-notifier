ed2dd26473007cad8d50ba682b501bb46bff9cfe
Jakub Kubacki <kubacki.jk@gmail.com>
2016-04-28 23:44:57 +0200

    Add sent monthly migration

---
+++ db/migrations/002_create_sent_monthly.rb
@@ -0,0 +1,13 @@
+Sequel.migration do
+  up do
+    create_table(:sent_monthly) do
+      primary_key :id
+      String :email, null: false, unique: true
+      Date :sent_at, null: false
+    end
+  end
+
+  down do
+    drop_table(:sent_monthly)
+  end
+end
